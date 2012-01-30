93e6cfa54dc1598fb5dfe12a65587c4bd80874cb
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2012-01-30 14:47:38 +0800

    new testcases added. Kata in python finished. Code is ugly

--- karate_chop/chop.py
+++ karate_chop/chop.py
@@ -1,7 +1,18 @@
 def chop(target, field):
   if len(field) == 0 :
     return -1
-  elif field[0] == target:
-    return 0
+  elif len(field) == 1:
+    if field[0] == target:
+      return 0
+    else:
+      return -1
   else:
-    return -1
+    m = len(field)//2
+    if target < field[m]:
+      return chop(target, field[:m])
+    else:
+      index = chop(target, field[m:])
+      if index == -1:
+        return -1
+      else:
+        return index + m
--- karate_chop/test_chop.py
+++ karate_chop/test_chop.py
@@ -15,5 +15,24 @@ class TestChop(unittest.TestCase):
     def test_one_element(self):
         self.assertEqual(0, chop(1, [1]))
 
+    def test_find_first(self):
+        self.assertEqual(0,  chop(1, [1, 3, 5]))
+
+    def test_find_middle(self):
+        self.assertEqual(1,  chop(3, [1, 3, 5]))
+
+    def test_find_last(self):
+        self.assertEqual(2,  chop(5, [1, 3, 5]))
+
+    def test_miss_before(self):
+        self.assertEqual(-1, chop(0, [1, 3, 5, 7]))
+
+    def test_miss_middle(self):
+        self.assertEqual(-1, chop(4, [1, 3, 5, 7]))
+
+    def test_miss_after(self):
+        self.assertEqual(-1, chop(8, [1, 3, 5, 7]))
+
+    
 if __name__ == '__main__':
     unittest.main()
