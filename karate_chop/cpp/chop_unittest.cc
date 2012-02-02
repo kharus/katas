eb25178b0a4a470b91358083e540b3ade90e39a9
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2012-02-02 17:11:56 +0800

    New testcase added

--- karate_chop/cpp/chop_unittest.cc
+++ karate_chop/cpp/chop_unittest.cc
@@ -2,9 +2,13 @@
 #include "chop.h"
 #include "gtest/gtest.h"
 
-
 TEST(ChopTest, EmptyField) {
   std::vector<int> v;
   EXPECT_EQ(-1, chop(5, v));
+}
+
+TEST(ChopTest, MissingSingle) {
+  std::vector<int> v = {1};
+  EXPECT_EQ(-1, chop(3, v));
 }
 
