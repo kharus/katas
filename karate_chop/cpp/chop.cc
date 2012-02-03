0b9d64c63319ba78a2bd7e16127feccfbfaa386b
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2012-02-03 19:50:47 +0800

    new testcases added. Kata finished.

--- karate_chop/cpp/chop.cc
+++ karate_chop/cpp/chop.cc
@@ -2,5 +2,25 @@
 #include <vector>
 
 int chop(int n, std::vector<int> field) {
-  return -1;
+  if (field.size() == 0) {
+    return -1;
+  }
+
+  int begin = 0;
+  int end = field.size() - 1;
+  
+  while (begin != end) {
+    int middle = (end - begin) / 2;
+    if ( n <= field[middle] ){
+      end = middle;
+    } else {
+      begin = middle + 1;
+    }
+  }
+
+  if ( field[begin] == n ) {
+    return begin;
+  } else {
+    return -1;
+  }
 }
--- karate_chop/cpp/chop_unittest.cc
+++ karate_chop/cpp/chop_unittest.cc
@@ -12,3 +12,39 @@ TEST(ChopTest, MissingSingle) {
   EXPECT_EQ(-1, chop(3, v));
 }
 
+TEST(ChopTest, FindSingle) {
+  std::vector<int> v = {1};
+  EXPECT_EQ(0, chop(1, v));
+}
+
+TEST(ChopTest, FindFirst) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(0, chop(1, v));
+}
+
+TEST(ChopTest, FindMiddle) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(1, chop(3, v));
+}
+
+TEST(ChopTest, FindLast) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(2, chop(5, v));
+}
+
+TEST(ChopTest, MissingBelow) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(-1, chop(0, v));
+}
+
+TEST(ChopTest, MissingMiddle) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(-1, chop(4, v));
+}
+
+TEST(ChopTest, MissingAbove) {
+  std::vector<int> v = {1, 3, 5};
+  EXPECT_EQ(-1, chop(7, v));
+}
+
+
